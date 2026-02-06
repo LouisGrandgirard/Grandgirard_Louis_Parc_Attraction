82bc068778502bc9de5356da9b56efa198a5375e
Louis Grandgirard <Grandgirard.Louis@univ-lorraine.fr>
2026-02-06 10:29:07 +0100

    Reset: Start from zero

--- python/sql_file/create.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (name, password, role) VALUES ('toto', 'toto', 'ADMIN');
-INSERT INTO users (name, password, role) VALUES ('admin', 'admin', 'ADMIN');
-
-INSERT INTO attraction (nom, description, difficulte, visible, author_id) VALUES ('Silver Star', 'Montagne russe', 3, 1, 1);
-INSERT INTO attraction (nom, description, difficulte, visible, author_id) VALUES ('Montagne 8', 'Montagne russe', 4, 1, 1);
